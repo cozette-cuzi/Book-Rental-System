917aeef730451ba5a8a983f7e8cce3609020dfe3
cozette-cuzi <cuzicozette@gmail.com>
2022-04-02 16:10:23 +0200

    initial commit

---
+++ database/migrations/2022_04_02_132021_create_books_table.php
@@ -0,0 +1,40 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateBooksTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('books', function (Blueprint $table) {
+            $table->id();
+            $table->string('name');
+            $table->string('authors ');
+            $table->string('description')->nullable();
+            $table->date('released_at');
+            $table->string('cover_image')->nullable();
+            $table->integer('pages');
+            $table->string('language_code')->default('hu');
+            $table->string('isbn')->unique();
+            $table->integer('in_stock ');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('books');
+    }
+}
